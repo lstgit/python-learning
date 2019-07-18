68bd0497152938b8e9cd81d8550ac1fe08ea6806
lisongtao <2419293762@qq.com>
2019-07-18 20:07:14 +0800

    git update

--- local-llst/file_test/file_test.py
+++ local-llst/file_test/file_test.py
@@ -1,23 +1,51 @@
 import os
-import log_test.logUtils
-
-
-def test():
-    pass
-
+import log_test.logUtils, re
 
 log = log_test.logUtils.Logger(file_name='info.log', level='debug').log
 
 
-if __name__ == '__main__':
-    test()
+def one_to_two(file_name, flag):
     log.info(os.getcwd())
-    log.debug(os.access(path='C:\\Users\\lisongtao\\PycharmProjects\\local-llst\\file_test', mode=os.F_OK))
-    file = open(file='C:\\Users\\lisongtao\\Desktop\\ab.txt', mode='r', encoding='utf8')
+    # log.debug(os.access(path=file_name, mode=os.F_OK))
+    file = open(file=file_name, mode='r', encoding='utf8')
     log.info(file)
-    log.info(file.read())
+    log.info('*' * 100)
+    result = file.read()
+    log.info(type(result))
+    log.info(result)
     file.close()
-    # with open(file='C:\\Users\\lisongtao\\Desktop\\ab.txt', mode='a', encoding='utf8') as file:
-    #     file.write('\n天下皆知美之为美，恶矣；天下皆知善之为善，斯不善矣。有无相生，难易相成，长短相形，高下相盈，音声相和，先后相随,恒也。'
-    #                '\n是以圣人处无为之事，行不言之教。万物作焉而不辞，生而不有，为而不恃，功成而弗居。夫唯弗居，是以不去。')
+    log.info(result)
+    if flag == 'zuh':
+        name = 'C:\\Users\\lisongtao\\PycharmProjects\\local-llst\\file_test\\zuh.log'
+        pattern = re.compile(r'data:ZxUserHistory (.*?),error:')
+    else:
+        name = 'C:\\Users\\lisongtao\\PycharmProjects\\local-llst\\file_test\\zdh.log'
+        pattern = re.compile(r'data:ZxDsHistory (.*?),error:')
+    rs = pattern.findall(result)
+    print(rs)
+    print(type(rs))
 
+    try:
+        with open(name, mode='a', encoding='utf8') as file:
+            for i in range(len(rs)):
+                # if i % 2 == 0:
+                print(rs[i])
+                rs[i].encode()
+                file.write(rs[i])
+                file.write('\n')
+                print('-' * 100)
+    except Exception as e:
+        print(e)
+    finally:
+        file.close()
+    log.info("done")
+    pass
+
+
+if __name__ == '__main__':
+    one_to_two('C:\\Users\\lisongtao\\Desktop\\data\\0618zdh.txt', 'zdh')
+    one_to_two('C:\\Users\\lisongtao\\Desktop\\data\\0618zuh.txt', 'zuh')
+    one_to_two('C:\\Users\\lisongtao\\Desktop\\data\\0619zdh.txt', 'zdh')
+    one_to_two('C:\\Users\\lisongtao\\Desktop\\data\\0619zuh.txt', 'zuh')
+    one_to_two('C:\\Users\\lisongtao\\Desktop\\data\\0620zdh.txt', 'zdh')
+    one_to_two('C:\\Users\\lisongtao\\Desktop\\data\\0620zuh.txt', 'zuh')
